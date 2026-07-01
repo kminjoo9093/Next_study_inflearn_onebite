301968da9918bd17bca8bf3c2ee7c2df08dbeeed
kminjoo9093 <kminjoo9093@naver.com>
2026-07-01 21:22:21 +0900

    feat: 페이지 라우팅 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,9 +1,11 @@
-// import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <header>글로벌 헤더</header>
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <header>글로벌 헤더</header>
+      <Component {...pageProps} />
+    </>
+  );
 }
